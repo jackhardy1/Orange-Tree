0d8dbec6adb1e77664f7c8856e2272d010839b4b
Jack Hardy <jackhardy1@gmail.com>
2016-04-08 15:07:27 +0100

    Orange Tree - with 2 year passes

--- spec/orange_tree_spec.rb
+++ spec/orange_tree_spec.rb
@@ -8,27 +8,32 @@ it "Tree should start with 50 oranges" do
 end
 
 it "Tree should be 1 metre high at start" do
-  orange_tree.height == 1
+  expect(orange_tree.height).to eq 1
 end
 
-describe "#pick_an_orange" do
+describe "pick_an_orange" do
 it "pick_an_orange reduces orange_count by 1" do
   expect{orange_tree.pick_an_orange}.to change{orange_tree.orange_count}.by(-1)
 end
 end
 
-describe "#water" do
+describe "water" do
   it "water the tree adds one orange" do
 expect{orange_tree.water}.to change{orange_tree.orange_count}.by(1)
   end
 end
 
-describe "#one_year_passes" do
+describe "one_year_passes" do
   it "When one_year_passes is called, 2 metres is added to height" do
     expect{orange_tree.one_year_passes}.to change{orange_tree.height}.by(2)
   end
+
   it "when one_year_passes is called, orange_count increases by 20" do
     expect{orange_tree.one_year_passes}.to change{orange_tree.orange_count}.by(20)
+  end
+
+  it "when the tree is two years old, it should have 90 oranges" do
+    expect{2.times do orange_tree.one_year_passes end}.to change{orange_tree.orange_count}.to eq 90
   end
 end
 end
